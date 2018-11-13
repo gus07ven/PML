d2d186e0039a883e9b5079e594d2b93e025dc43b
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2018-11-13 00:30:04 -0600

    Modify logistic regression

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -2,6 +2,8 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from Regions import plot_decision_regions
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Load up data
@@ -21,3 +23,10 @@ X_test_std = sc.transform(X_test)
 # Train logistic regression model
 log_reg = LogisticRegression(C=1000, random_state=0)
 log_reg.fit(X_train_std, y_train)
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=log_reg, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
